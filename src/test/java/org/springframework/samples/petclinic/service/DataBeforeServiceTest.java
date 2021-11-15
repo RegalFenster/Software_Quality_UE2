7876190772034089cba5346a9b3b8a6ca6e272df
Fisnik Beqiri <fisnik.beqiri@edu.fh-joanneum.at>
2021-11-15 16:48:41 +0100

    Part03 - Add Visits Test

--- src/test/java/org/springframework/samples/petclinic/service/DataBeforeServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/service/DataBeforeServiceTest.java
@@ -1,4 +1,5 @@
 package org.springframework.samples.petclinic.service;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,9 +7,13 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.samples.petclinic.owner.*;
+import org.springframework.samples.petclinic.visit.Visit;
+import org.springframework.samples.petclinic.visit.VisitRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,18 +24,20 @@ import static org.mockito.BDDMockito.given;
 // Ensure that if the mysql profile is active we connect to the real database:
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DataBeforeServiceTest {
-	Pet[] petsArr;
+	List<Pet> petsList;
 
 	@Autowired
 	protected OwnerRepository owners;
 	@Autowired
 	protected PetRepository pets;
+	@Autowired
+	protected VisitRepository visits;
 
 	@Transactional
 	@BeforeEach
 	void setup(){System.out.println("Setting up");
 
-		petsArr = new Pet[10];
+		petsList = new ArrayList<Pet>();
 
 		Owner owner=new Owner();
 		owner.setFirstName("Test");
@@ -47,8 +54,8 @@ public class DataBeforeServiceTest {
 			type.setName(pet.getName()+i);
 			pet.setType(type);
 			owner.addPet(pet);
+			petsList.add(pet);
 			this.pets.save(pet);
-
 		}
 	}
 
@@ -56,12 +63,27 @@ public class DataBeforeServiceTest {
 	@Test
 	void listAllOrderedPets(){
 		List<PetType> petTypes=pets.findPetTypes();
-
-		for(int i=0;i<petTypes.size();i++){
-			System.out.println(petTypes.get(i));}
 		assertThat(petTypes.get(5).getName()).contains("name00");
 		assertThat(petTypes.get(6).getName()).contains("name11");
 		assertThat(petTypes.get(7).getName()).contains("name22");
 	}
 
+	@Transactional
+	@Test
+	void checkForVisits() {
+
+		for(int j=0;j<3;j++){
+			Visit v = new Visit();
+			v.setPetId(petsList.get(j).getId());
+			v.setDescription(String.format("Visit number: %d for Pet: %s",v.getId(),petsList.get(j).getName()));
+			petsList.get(j).addVisit(v);
+			this.visits.save(v);
+			this.pets.save(petsList.get(j));
+			System.out.println(petsList.get(j));
+		}
+
+		assertThat(petsList.get(0).getVisits().size()).isGreaterThan(0);
+		assertThat(petsList.get(1).getVisits().size()).isLessThan(2).isGreaterThan(0);
+		assertThat(petsList.get(2).getVisits().size()).isGreaterThanOrEqualTo(1);
+	}
 }
